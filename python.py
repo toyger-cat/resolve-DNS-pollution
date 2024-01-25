257d88824f0db7327928d9de68018eeef7e7b848
Charles Frazier <water-pill@outlook.com>
2024-01-25 19:32:14 +0800

    Update python.py

--- python.py
+++ python.py
@@ -1,22 +1,22 @@
-# !pip install requests beautifulsoup4 pyinstaller
 import sys
 import os
 import requests
 from bs4 import BeautifulSoup
 import re
-if os.geteuid() != 0:
-    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-        print('Please run with sudo')
-        sys.exit(1)
-    elif sys.platform.startswith('win'):
-        print('Please run with administrator')
-        sys.exit(1)
-    else:
-        print('Cannot detect your platform')
-        sys.exit(1)
-str = ''
+
+def check_platform():
+    if os.geteuid() != 0:
+        if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+            print('Please run with sudo')
+            sys.exit(1)
+        elif sys.platform.startswith('win'):
+            print('Please run with administrator')
+            sys.exit(1)
+        else:
+            print('Cannot detect your platform')
+            sys.exit(1)
+
 def get_ip_address(url, name):
-    global str
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     links = soup.find_all('a')
@@ -25,10 +25,9 @@ def get_ip_address(url, name):
         href = link.get('href')
         if href and pattern.match(href):
             ip_address = pattern.match(href).group(1)
-            str = str + ip_address + ' ' + name + '\n'
-            break
-get_ip_address('https://sites.ipaddress.com/github.com', 'github.com')
-get_ip_address('https://sites.ipaddress.com/github.global.ssl.fastly.net', 'github.global.ssl.fastly.net')
+            return ip_address + ' ' + name + '\n'
+    return ''
+
 def clean_hosts(hosts_path):
     with open(hosts_path,'r+') as f:
         lines = f.readlines()
@@ -43,14 +42,23 @@ def clean_hosts(hosts_path):
                 continue
             else:
                 f.write(line)
+
 def prepend_to_hosts(content, hosts_path):
     clean_hosts(hosts_path)
     with open(hosts_path, 'r+') as f:
         original_content = f.read()
         f.seek(0)
         f.write(content + original_content)
+        print('Done')
+
+check_platform()
+
+ip_addresses = ''
+ip_addresses += get_ip_address('https://sites.ipaddress.com/github.com', 'github.com')
+ip_addresses += get_ip_address('https://sites.ipaddress.com/github.global.ssl.fastly.net', 'github.global.ssl.fastly.net')
+
 if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-    prepend_to_hosts(str, '/etc/hosts')
+    prepend_to_hosts(ip_addresses, '/etc/hosts')
 elif sys.platform.startswith('win'):
-    prepend_to_hosts(str, 'C:\\Windows\\System32\\drivers\\etc\\hosts')
-# pyinstaller --onefile python.py
+    prepend_to_hosts(ip_addresses, 'C:\\Windows\\System32\\drivers\\etc\\hosts')
+    os.system('ipconfig /flushdns')
