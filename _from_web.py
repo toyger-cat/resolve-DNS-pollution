34b1acbe539f02c7fb37bcf386bb05bd80019788
water-pill <water-pill@outlook.com>
2024-01-30 11:52:20 +0800

    Update _from_web.py

--- _from_web.py
+++ _from_web.py
@@ -1,88 +1,75 @@
+import itertools
+import threading
 import sys
 import os
 import requests
-from bs4 import BeautifulSoup
 import re
-import ctypes
+from bs4 import BeautifulSoup
+import time
+
+done = False
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rUpdating hosts file ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+t = threading.Thread(target=animate)
 
 def is_admin():
     if os.name == 'nt':
+        import ctypes
         return ctypes.windll.shell32.IsUserAnAdmin()
     else:
         return os.geteuid() == 0
 
-def get_ip_address(url, name):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        div = soup.find('div', {'id': 'tabpanel-dns-a'})
-        if div:
-            pattern = re.compile(r'https://www.ipaddress.com/ipv4/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-            link = div.find('a', href=pattern)
-            if link:
-                ip_address = pattern.match(link['href']).group(1)
-                return ip_address + ' ' + name + '\n'
-    except Exception as e:
-        print(f"Error occurred while getting IP address: {e}")
+def get_ip_address(name):
+    url = 'https://sites.ipaddress.com/' + name
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+    pattern = re.compile(r'https://www.ipaddress.com/ipv4/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    for link in links:
+        href = link.get('href')
+        if href and pattern.match(href):
+            ip_address = pattern.match(href).group(1)
+            return ip_address + ' ' + name + '\n'
     return ''
 
 def clean_hosts(hosts_path):
-    try:
-        with open(hosts_path,'r+') as f:
-            lines = f.readlines()
-            f.seek(0)
-            f.truncate()
-            preserved_lines = [line for line in lines if not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+(github.com|github.global.ssl.fastly.net|assets-cdn.github.com)', line) and line != '\n']
-            f.writelines(preserved_lines)
-    except Exception as e:
-        print(f"Error occurred while cleaning hosts: {e}")
-        sys.exit(1)
+    with open(hosts_path,'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.truncate()
+        preserved_lines = [line for line in lines if not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+(github.com|github.global.ssl.fastly.net|assets-cdn.github.com)', line) and line != '\n']
+        f.writelines(preserved_lines)
 
 def prepend_to_hosts(content, hosts_path):
-    try:
-        clean_hosts(hosts_path)
-        with open(hosts_path, 'r+') as f:
-            original_content = f.read()
-            f.seek(0)
-            f.write(content + original_content)
-            print('---------------------Done!--------------------')
-            print('Your hosts file has been updated as below:')
-            print(content + original_content)
-            print('Your hosts file is located at: ' + hosts_path + '.  You can restore it manually if you want to')
-    except Exception as e:
-        print(f"Error occurred while updating hosts: {e}")
-        sys.exit(1)
-
-def flush_dns():
-    if sys.platform.startswith('win'):
-        os.system('ipconfig /flushdns')
-
-def exit_prompt():
-    input("Press Enter to exit...")
-    sys.exit(0)
+    clean_hosts(hosts_path)
+    with open(hosts_path, 'r+') as f:
+        original_content = f.read()
+        f.seek(0)
+        f.write(content + original_content)
+        print('\n---------------------Done!--------------------')
+        print('Your hosts file has been updated as below:\n')
+        print(content + original_content)
+        print('Your hosts file is located at: ' + hosts_path + '.  You can restore it manually if you want to.')
 
 if not is_admin():
     print("please use 'administrator privileges' or 'sudo' to run this script")
-    sys.exit(1)
-
-while True:
-    choice = input("Please choose to clean previous hosts file settings or update hosts file (C/U): ")
-    if choice not in ['C', 'U']:
-        print('Invalid input, please try again')
-        continue
-    hosts_path = '/etc/hosts' if sys.platform.startswith('linux') or sys.platform.startswith('darwin') else 'C:\\Windows\\System32\\drivers\\etc\\hosts'
-    if choice == 'C':
-        clean_hosts(hosts_path)
-        flush_dns()
-        print('---------------------Done!--------------------')
-        print('Your hosts file has been cleaned up successfully')
-        exit_prompt()
-    elif choice == 'U':
-        ip_addresses = ''
-        ip_addresses += get_ip_address('github.com')
-        ip_addresses += get_ip_address('github.global.ssl.fastly.net')
-        if sys.platform.startswith('win'):
-            ip_addresses += get_ip_address('assets-cdn.github.com')
-        prepend_to_hosts(ip_addresses, hosts_path)
-        flush_dns()
-        exit_prompt()
+    input("Press Enter to exit...")
+    sys.exit(0)
+hosts_path = '/etc/hosts' if sys.platform.startswith('linux') or sys.platform.startswith('darwin') else 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+t.start()
+ip_addresses = ''
+ip_addresses += get_ip_address('github.com')
+ip_addresses += get_ip_address('github.global.ssl.fastly.net')
+if sys.platform.startswith('win'):
+    ip_addresses += get_ip_address('assets-cdn.github.com')
+prepend_to_hosts(ip_addresses, hosts_path)
+if sys.platform.startswith('win'):
+    os.system('ipconfig /flushdns')
+done = True
+input("Press Enter to exit...")
+sys.exit(0)
